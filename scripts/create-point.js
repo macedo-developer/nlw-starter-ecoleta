e4864910de550b7cc470bea0e47378957030730e
macedo-developer <macedorenata.developer@gmail.com>
2020-06-03 14:50:03 -0300

    Create a collection point and JavaScript registration page to obtain IBGE API states and cities

---
+++ scripts/create-point.js
@@ -0,0 +1,37 @@
+document.querySelector("select[name=uf]").addEventListener("change", getCities);
+
+function populateUfs() {
+  const ufSelect = document.querySelector("select[name=uf]");
+
+  fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    .then((response) => response.json())
+    .then((states) => {
+      for (state of states) {
+        ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`;
+      }
+    });
+}
+
+populateUfs();
+
+function getCities(event) {
+  const citySelect = document.querySelector("select[name=city]");
+  const stateInput = document.querySelector("[name=state]");
+
+  const ufValue = event.target.value;
+
+  const indexOfSelectedState = event.target.selectedIndex;
+  stateInput.value = event.target.options[indexOfSelectedState].text;
+
+  const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((cities) => {
+      for (city of cities) {
+        citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`;
+      }
+
+      citySelect.disabled = false;
+    });
+}
