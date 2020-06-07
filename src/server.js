0e5010ca9f81890bb07c5b031d33df2ce63d2299
macedo-developer <macedorenata.developer@gmail.com>
2020-06-07 15:22:35 -0300

    Using template engine nunjucks

--- src/server.js
+++ src/server.js
@@ -4,12 +4,21 @@ const server = express();
 
 server.use(express.static("public"));
 
+//Utilizando template engine
+const nunjucks = require("nunjucks");
+
+nunjucks.configure("src/views", { express: server, noCache: true });
+
 server.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+  return res.render("index.html");
 });
 
 server.get("/create-point", (req, res) => {
-  res.sendFile(__dirname + "/views/create-point.html");
+  return res.render("create-point.html");
+});
+
+server.get("/search", (req, res) => {
+  return res.render("search-results.html");
 });
 
 server.listen(3000);
